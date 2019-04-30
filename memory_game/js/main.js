a86912d73aee9c23be342f955dd46931783fd3cf
adam-weiler <adam.weiler@yahoo.ca>
2019-04-30 17:53:59 -0400

    Created game logic. (Control Flow unit.)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
 console.log('Up and running!');
 
-let cardOne = 'queen';
-let cardTwo = 'king';
-let cardThree = 'queen';
-let cardFour = 'king';
+let cards = ['queen', 'queen', 'king', 'king']; //These are our cards.
 
+let cardsInPlay = []; //Stores which cards the user has tried.
+
+let cardOne = cards[0]; //User's first pick is first card.
+cardsInPlay.push(cardOne); //Pushes first pick onto their array.
 console.log(`User flipped ${cardOne}`);
-console.log(`User flipped ${cardThree}`);
+
+let cardTwo = cards[1]; //User's second pick is third card.
+cardsInPlay.push(cardTwo); //Pushes second pick onto their array.
+console.log(`User flipped ${cardTwo}`);
+
+if (cardsInPlay.length === 2) { //Checks if user has picked two cards.
+
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+
+}
