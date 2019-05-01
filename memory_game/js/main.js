38e16268efc40ad7701d3466fe7b5baf5cc292c0
adam-weiler <adam.weiler@yahoo.ca>
2019-05-01 15:30:01 -0400

    Dividing code into functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,22 @@
-console.log('Up and running!');
-
 let cards = ['queen', 'queen', 'king', 'king']; //These are our cards.
-
 let cardsInPlay = []; //Stores which cards the user has tried.
 
-let cardOne = cards[0]; //User's first pick is first card.
-cardsInPlay.push(cardOne); //Pushes first pick onto their array.
-console.log(`User flipped ${cardOne}`);
-
-let cardTwo = cards[1]; //User's second pick is third card.
-cardsInPlay.push(cardTwo); //Pushes second pick onto their array.
-console.log(`User flipped ${cardTwo}`);
-
-if (cardsInPlay.length === 2) { //Checks if user has picked two cards.
-
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
+function checkForMatch() { //Checks if first card and second card are a match.
+    if (cardsInPlay[0] === cardsInPlay[1]) { //If both cards match.
+        console.log("You found a match!");
+    } else { //Else they do not match.
+        console.log("Sorry, try again.");
     }
-
 }
+
+function flipCard(cardId) { //When user flips a card.
+    console.log(`User flipped ${cards[cardId]}.`); //Logs face value of current card picked.
+    cardsInPlay.push(cards[cardId]); //Pushes current card picked onto cardsInPlay array.
+    
+    if (cardsInPlay.length === 2) { //Checks if user has picked two cards.
+        checkForMatch();
+    }
+}
+
+flipCard(0); //Simulates user flipping card.
+flipCard(2); //Simulates user flipping card.
